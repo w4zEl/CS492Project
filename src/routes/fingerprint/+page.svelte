f313bf1f5b712fc81c686c49f8cd6642063f5f87
w4zEl <proximitytrigger@gmail.com>
2025-04-01 01:06:31 -0400

    Add battery status.

--- src/routes/fingerprint/+page.svelte
+++ src/routes/fingerprint/+page.svelte
@@ -2,16 +2,27 @@
 	import * as Table from '$lib/components/ui/table';
 	import Info from '@lucide/svelte/icons/info';
 	import * as Tooltip from '$lib/components/ui/tooltip';
-    import FingerprintJS from '@fingerprintjs/fingerprintjs'
+	import FingerprintJS from '@fingerprintjs/fingerprintjs';
 	import { onMount } from 'svelte';
+	import { Progress } from '$lib/components/ui/progress';
+	import { Switch } from '$lib/components/ui/switch';
 	const { data } = $props();
 	let fingerprint = $state<string>();
-    onMount(() => {
-        FingerprintJS.load().then(fp => fp.get()).then(res => {
-            console.log(res);
-            fingerprint = res.visitorId;
-        });
-    });
+	let charging = $state(false);
+	let chargeLevel = $state<number>();
+	onMount(() => {
+		navigator.getBattery().then((battery) => {
+			charging = battery.charging;
+			chargeLevel = battery.level;
+			battery.addEventListener('chargingchange', () => {
+				charging = battery.charging;
+				chargeLevel = battery.level;
+			});
+		});
+		FingerprintJS.load()
+			.then((fp) => fp.get())
+			.then((res) => (fingerprint = res.visitorId));
+	});
 </script>
 
 <div class="p-4">
@@ -36,7 +47,7 @@
 	<p class="underline">Your device's fingerprint is:</p>
 
 	<div class="font-mono">
-        {#if fingerprint}
+		{#if fingerprint}
 			{fingerprint}
 		{:else}
 			<svg
@@ -44,18 +55,19 @@
 				class="inline h-8 w-8 animate-spin fill-green-500 text-gray-200 dark:text-gray-600"
 				viewBox="0 0 100 101"
 				fill="none"
-				xmlns="http://www.w3.org/2000/svg">
-                <path
+				xmlns="http://www.w3.org/2000/svg"
+			>
+				<path
 					d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
 					fill="currentColor"
 				/>
-                <path
+				<path
 					d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
 					fill="currentFill"
 				/>
-            </svg>
+			</svg>
 		{/if}
-    </div>
+	</div>
 
 	<h2 class="my-[0.83em] text-xl font-semibold leading-snug">Details of your Device</h2>
 	<Table.Root>
@@ -197,6 +209,26 @@
 					<Table.Cell
 						>{'deviceMemory' in navigator ? navigator.deviceMemory + ' GB' : 'unknown'}</Table.Cell
 					>
+				</Table.Row>
+				<Table.Row>
+					<Table.Cell>Battery Status</Table.Cell>
+					<Table.Cell>
+						{#if chargeLevel !== undefined}
+							<div class="mb-2 flex gap-2">
+								Charging: <Switch checked={charging} aria-readonly class="pointer-events-none" />
+							</div>
+							<div class="flex gap-2">
+								<div class="flex-shrink-0">Battery level:</div>
+								<Progress
+									value={chargeLevel * 100}
+									title={Math.round(chargeLevel * 100) + '%'}
+									class="max-w-sm"
+								/>
+							</div>
+						{:else}
+							Unknown
+						{/if}
+					</Table.Cell>
 				</Table.Row>
 			{/if}
 		</Table.Body>
