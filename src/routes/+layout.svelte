65d4662d5ea175f3d1b9b1b3acb256a1d8b6fc14
w4zEl <proximitytrigger@gmail.com>
2025-04-04 23:39:24 -0400

    Reformat root layout.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,7 @@
 	import '../app.css';
 	import { Button } from '$lib/components/ui/button';
 	import { fly } from 'svelte/transition';
-	import * as Sidebar from "$lib/components/ui/sidebar/index.js";
+	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
 	import AppSidebar from '$lib/components/app-sidebar.svelte';
 	let { children, data } = $props();
 	let cookiesConsent = $state(data.cookiesConsent);
@@ -11,19 +11,23 @@
 <Sidebar.Provider>
 	<AppSidebar />
 	<main>
-	  <Sidebar.Trigger />
-	  {@render children?.()}
+		<Sidebar.Trigger />
+		{@render children?.()}
 	</main>
-  </Sidebar.Provider>
+</Sidebar.Provider>
 
 {#if !cookiesConsent}
 	<div
-		class="z-20 fixed bottom-4 left-4 right-4 flex items-center justify-between rounded-lg bg-white p-4 shadow-lg"
+		class="fixed bottom-4 left-4 right-4 z-20 flex items-center justify-between rounded-lg bg-white p-4 shadow-lg"
 		transition:fly={{ y: 200, duration: 2000 }}
 	>
-		<p class="text-sm" >
+		<p class="text-sm">
 			This website uses cookies to enhance your browsing experience. By continuing to use our
-			website, you agree to our <a href="/privacy-policy" class="text-blue-600 hover:text-blue-800 transition-colors duration-200 underline underline-offset-2 focus:outline-none focus:ring-2 focus:ring-blue-400">Privacy Policy</a>.
+			website, you agree to our <a
+				href="/privacy-policy"
+				class="text-blue-600 underline underline-offset-2 transition-colors duration-200 hover:text-blue-800 focus:outline-none focus:ring-2 focus:ring-blue-400"
+				>Privacy Policy</a
+			>.
 		</p>
 		<Button
 			variant="outline"
